d9e6bbf67c1fac56fbe1527024db963a213ed3c9
Brian Gershon <brian.five@gmail.com>
2018-05-14 15:10:32 -0700

    Retrieve list of all projects and save to file.

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Requires NodeJS 7.6+ (for async/await support).
     export GITLAB_TOKEN=123
     export GITLAB_INSTANCE=https://gitlab.example.com
     npm start
+    # creates a file called all-projects.json
+
+    # lookup a project
+    npm run list-projects | grep dashboard
--- server.js
+++ server.js
@@ -1,26 +1,41 @@
-var axios = require('axios');
-var parse = require('parse-link-header');
+const axios = require('axios');
+const parse = require('parse-link-header');
+const fs = require('fs');
 
 GITLAB_TOKEN = process.env.GITLAB_TOKEN;
 GITLAB_INSTANCE = process.env.GITLAB_INSTANCE;
 
-async function getProjects() {
+async function getAllProjects() {
   const options = {
     headers: {
       'Private-Token': GITLAB_TOKEN
     }
   };
 
-  const response = await axios.get(`${GITLAB_INSTANCE}/api/v4/projects?per_page=100`, options);
-  return response;
+  const projects = [];
+
+  let url = `${GITLAB_INSTANCE}/api/v4/projects?per_page=100`;
+
+  while (url) {
+    const response = await axios.get(url, options);
+    const result = response.data;
+    result.map((item) => {
+      projects.push({ id: item.id, name: item.name, name_with_namespace: item.name_with_namespace });
+    });
+    const next = parse(response.headers['link'])['next'];
+    if (next) {
+      url = next.url;
+    } else {
+      url = '';
+    }
+  }
+  return projects;
 }
 
-getProjects().then((response) => {
-  const result = response.data;
-  console.log(parse(response.headers['link'])['next'].url);
-  console.log(result.map((item) => {
-     return { id: item.id, name: item.name, name_with_namespace: item.name_with_namespace };
-   }));
+getAllProjects().then((projects) => {
+   fs.writeFile('all-projects.json', JSON.stringify(projects), 'utf8', () => {
+     console.log('wrote all-projects.json');
+   });
 }).catch((err) => {
   console.log('err', err);
 });
