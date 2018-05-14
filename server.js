2efc0a0d33a5ae066de03ccf81f0f38a0b9cf0e9
Brian Gershon <brian.five@gmail.com>
2018-05-14 16:19:45 -0700

    Add figlet

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
+
 const fs = require('fs');
 const program = require('commander');
+const figlet = require('figlet');
 const { getAllProjects } = require('./src/api-projects');
 
 const { GITLAB_TOKEN, GITLAB_INSTANCE } = process.env;
 
+console.log(figlet.textSync('gitlab-play', { horizontalLayout: 'full' }));
+
 program
   .version('1.0.0')
-  .description('Play with Gitlab API');
+  .description('Play with Gitlab API.');
 
 program
   .command('save-all-projects')
